f06eb1e10f2f6b05d4c9e6777e46266646c0d65b
EJD222 <eljdiaz500@gmail.com>
2026-09-12 23:10:08 +0800

    feat(workspace): add workspace feature module

--- apps/api/src/features/features.module.ts
+++ apps/api/src/features/features.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
 
 @Module({
     imports: [
         AuthModule,
+        WorkspacesModule,
     ],
     exports: [
         AuthModule,
